571e92bb0a8b437e86421fa7bd830e75dd105d45
kovey <kovey@vip.qq.com>
2020-11-26 13:27:53 +0800

    bugs fixed

--- src/App/Bootstrap/Bootstrap.php
+++ src/App/Bootstrap/Bootstrap.php
@@ -36,7 +36,10 @@ class Bootstrap
     public function __initLogger(Application $app)
     {
         ko_change_process_name(Manager::get('server.rpc.name') . ' rpc root');
-        Logger::setLogPath(Manager::get('server.logger.info'), Manager::get('server.logger.exception'), Manager::get('server.logger.error'), Manager::get('server.logger.warning'));
+        Logger::setLogPath(
+            Manager::get('server.logger.info'), Manager::get('server.logger.exception'), Manager::get('server.logger.error'), 
+            Manager::get('server.logger.warning'), Manager::get('server.logger.busi_exception')
+        );
         Logger::setCategory(Manager::get('server.rpc.name'));
         Db::setLogDir(Manager::get('server.logger.db'));
         Monitor::setLogDir(Manager::get('server.logger.monitor'));
